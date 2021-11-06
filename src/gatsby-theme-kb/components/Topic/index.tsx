fe07227f1df82a0ba1026f145380e4d3e3306ae1
Jatin <jatin.nagpal222@gmail.com>
2021-11-07 02:11:51 +0530

    comment out comments, should work now

--- src/gatsby-theme-kb/components/Topic/index.tsx
+++ src/gatsby-theme-kb/components/Topic/index.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import ThemeTopic, {
   Props as ThemeTopicProps,
 } from 'gatsby-theme-kb/src/components/Topic'
-// import Comment from './Comment'
+import Comment from './Comment'
 
 interface TopicProps extends ThemeTopicProps {}
 
-// const Topic = (props: TopicProps) => {
-//   return (
-//     <>
-//       <ThemeTopic {...props}></ThemeTopic>
-//       <div className="comment-wrapper">
-//         <Comment issueTerm={props.file.fields.slug} />
-//       </div>
-//     </>
-//   )
-// }
+const Topic = (props: TopicProps) => {
+  return (
+    <>
+      <ThemeTopic {...props}></ThemeTopic>
+      <div className="comment-wrapper">
+        <Comment issueTerm={props.file.fields.slug} />
+      </div>
+    </>
+  )
+}
 
 export default Topic
